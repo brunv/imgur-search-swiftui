5394c503561e072894dd2375346b03def785ded9
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2021-09-04 08:05:10 -0300

    Initial Commit

---
+++ imgur-search/imgur_searchApp.swift
@@ -0,0 +1,17 @@
+//
+//  imgur_searchApp.swift
+//  imgur-search
+//
+//  Created by Bruno Vieira on 04/09/21.
+//
+
+import SwiftUI
+
+@main
+struct imgur_searchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
