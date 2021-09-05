b3adcd4716c94f9fbeb19f3c4b74b3cad56dc8a0
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2021-09-05 11:06:29 -0300

    Remove comentários

--- imgur-search/Views/HomeView.swift
+++ imgur-search/Views/HomeView.swift
@@ -30,59 +30,6 @@ struct HomeView: View {
             }.padding()
             ImageGridView(images: imageViewModel.images)
         }
-//            VStack(spacing: 0) {
-//                HStack {
-//                    if !searchBarIsExpanded {
-//                        VStack(alignment: .leading, spacing: 8) {
-//                            Text("Imgur Search")
-//                                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-//                                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-//                        }.foregroundColor(.black)
-//                    }
-//                    Spacer(minLength: 0)
-//                    Image(systemName: "magnifyingglass")
-//                        .foregroundColor(.gray)
-//                        .onTapGesture {
-//                            withAnimation {
-//                                searchBarIsExpanded = true
-//                            }
-//                        }
-//                    if searchBarIsExpanded {
-//                        TextField("Search...", text: $search)
-//
-//                        Button(action: {
-//                            withAnimation {
-//                                searchBarIsExpanded = false
-//                            }
-//                        }, label: {
-//                            Image(systemName: "xmark")
-//                                .font(.system(size: 15, weight: .bold))
-//                                .foregroundColor(.black)
-//                        })
-//                        .padding(.leading, 10)
-//
-//                        if !search.isEmpty {
-//                            Button(action: {
-//                                imageViewModel.fetchImages(for: self.search)
-//                            }, label: {
-//                                Text("     GO")
-//                                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-//                                    .foregroundColor(.black)
-//                            })
-//                        }
-//
-//                    }
-//                }
-//                .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
-//                .padding()
-//                .background(Color.white)
-//
-//                Spacer()
-//
-//                ImageGridView(images: imageViewModel.images)
-//            }
-//            .background(Color.black.opacity(0.08).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/))
-//            .edgesIgnoringSafeArea(.top)
     }
 }
 
